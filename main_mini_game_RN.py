9507279bb17e2b939b8ed0b522c345c1ea99f4ef
Vladislav Razvodov <Gerhard8RVA@gmail.com>
2026-01-18 14:41:17 +0300

    feat: add guess number game

---
+++ main_mini_game_RN.py
@@ -0,0 +1,22 @@
+import random
+secret = random.randint(1, 10)
+while True:
+    guess = input("I thought of a number between 1 and 10. Try to guess: ")
+    try:
+        guess = int(guess)
+        if guess > secret:
+            print('Too big. Try again.')
+        elif guess < secret:
+            print('Too small. Try again.')
+        else:
+            print('Congratulations! You guessed it!')
+            ag = input('Shall we play again?: Y/N').strip().lower()
+            if ag == 'y':
+                secret = random.randint(1, 10)
+                continue
+            else:
+                print('Bye!')
+                break
+    except ValueError:
+        print('You had to enter a number')
+        continue
